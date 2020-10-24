c3fa9a389550544acf181a561e42ed36aa99255e
Raphaël Jean <raphael.jean@rocketmail.com>
2020-10-24 14:13:10 -0400

    Fixed namespace

--- rtcom/__init__.py
+++ rtcom/__init__.py
@@ -2,4 +2,7 @@
 
 __author__ = """Raphael Jean"""
 __email__ = 'raphael.jean@rocketmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.2'
+
+from .rtcom import RealTimeCommunication, RealTimeCommunicationWriter, RealTimeCommunicationListener
+
