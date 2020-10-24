5614940dcb0874e3c8d640877c1e2c69bf508f8b
Raphaël Jean <raphael.jean@rocketmail.com>
2020-10-24 15:11:26 -0400

    Fixed documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,10 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import rtcom
 
+import sphinx_rtd_theme
+
+
+
 # -- General configuration ---------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -31,7 +35,7 @@ import rtcom
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -83,8 +87,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
