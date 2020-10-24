e4d13ca1986808d2752953311c8cb5fdd84aa5a3
Raphaël Jean <raphael.jean@rocketmail.com>
2020-10-24 17:09:54 -0400

    Examples

---
+++ examples/video_reader.py
@@ -0,0 +1,32 @@
+from rtcom import RealTimeCommunication
+import cv2
+import numpy as np
+from time import sleep
+
+with RealTimeCommunication("pc") as rtcom:
+    rtcom.subscribe("rpi","camera") #Request unicast for this specific endpoint.
+    #(Too much packet drop on multicast for some reason.)
+    print("Press escape to quit.")
+    while True:
+        try:
+            image_data = rtcom["rpi"]["camera"]
+            if image_data is not None:
+                jpg_data = np.asarray(image_data)
+                img = cv2.imdecode(jpg_data, cv2.IMREAD_UNCHANGED)
+                write_header(img, "Video Feed")
+                data = rtcom["rpi"]["data"]
+                for i, name in enumerate(data):
+                    write_line(img, i, f"{name} : {data[name][0]:0.1f} {data[name][1]}")                
+                cv2.imshow("preview", img) 
+
+            key = cv2.waitKey(20)
+            if key==-1: 
+                key=None
+            else:
+                key=chr(key)
+
+            if key==27: #Escape key
+                break
+        except KeyError:
+            pass
+
---
+++ examples/video_writer.py
@@ -0,0 +1,28 @@
+import io
+import time
+import cv2
+
+from rtcom import RealTimeCommunication
+from PIL import Image, ImageDraw
+import numpy as np
+from threading import Thread
+
+from utils import VideoCapture, write_header, write_line
+
+import cv2, queue, threading, time
+
+with RealTimeCommunication("rpi") as rtcom:
+    cap = VideoCapture(0)
+    first_pass = True 
+    data = {}
+    loop_start_time=0
+
+    while(True):
+        data["Cycle Time"] = ((time.perf_counter() - loop_start_time)*1000, "ms")
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 70]
+        frame = cap.read()
+        loop_start_time=time.perf_counter()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        ret, jpg_image = cv2.imencode("*.jpg",frame, encode_param)
+        rtcom.broadcast_endpoint("camera", bytes(jpg_image), encoding="binary")
+        
