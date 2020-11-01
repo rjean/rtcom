aa65bed1a9a28da830db4c044425f090f3eff947
Raphaël Jean <raphael.jean@rocketmail.com>
2020-11-01 18:47:33 -0500

    Will ignore late packets

--- rtcom/__init__.py
+++ rtcom/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Raphael Jean"""
 __email__ = 'raphael.jean@rocketmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .rtcom import RealTimeCommunication, RealTimeCommunicationWriter, RealTimeCommunicationListener
 
